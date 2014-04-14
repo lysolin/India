0f6ed241b3598e45eefb69e0b62989a0c38189d3
Lysolin <lysolin@lysolin.com>
2014-04-14 11:40:38 -0700

    the very first commit

---
+++ src/com/main/Application.java
@@ -0,0 +1,13 @@
+package com.main;
+
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing 1 2 3...");
+	}
+
+}
