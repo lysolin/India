1100e70bda1e83e7d32618d0e5668721e1409e77
Lysolin <lysolin@lysolin.com>
2014-04-14 12:24:52 -0700

    company information

--- src/com/main/Application.java
+++ src/com/main/Application.java
@@ -1,5 +1,6 @@
 package com.main;
 
+import com.model.Company;
 import com.model.User;
 
 public class Application {
@@ -11,7 +12,9 @@ public class Application {
 		// TODO Auto-generated method stub
 		//System.out.println("Testing 1 2 3...");
 		User user = new User("David", "Bowie");
+		Company company = new Company("Transactional");
 		System.out.println("Owner: "+user.getFirstName()+" "+user.getLastName());
+		System.out.println("Works in: "+company.getCompanyName());
 	}
 
 }
