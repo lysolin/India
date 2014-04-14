6dec7d6f0e6d8aca05c3b9ef77d91b699c9b3158
Lysolin <lysolin@lysolin.com>
2014-04-14 12:20:51 -0700

    company class

---
+++ src/com/model/Company.java
@@ -0,0 +1,20 @@
+package com.model;
+
+public class Company {
+	
+	private String companyName;
+
+	public Company(String companyName) {
+		super();
+		this.companyName = companyName;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	
+}
